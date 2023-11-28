768c323b8405472d9e3aee7f69d82144cc188b7e
Gao Sun <gao@silverhand.io>
2023-11-28 16:17:04 +0800

    chore: add organization doc link

--- src/Constants/UserScope.php
+++ src/Constants/UserScope.php
@@ -17,8 +17,16 @@ enum UserScope: string
   case identities = 'identities';
   /** Scope for user's roles (`roles` claim). */
   case roles = 'roles';
-  /** Scope for user's organization IDs and perform organization token grant per [RFC 0001](https://github.com/logto-io/rfcs) (`organizations` claim). */
+  /** 
+   * Scope for user's organization IDs and perform organization token grant per [RFC 0001](https://github.com/logto-io/rfcs) (`organizations` claim).
+   *
+   * To learn more about Logto Organizations, see https://docs.logto.io/docs/recipes/organizations/.
+   */
   case organizations = 'urn:logto:scope:organizations';
-  /** Scope for user's organization roles per [RFC 0001](https://github.com/logto-io/rfcs) (`organization_roles` claim). */
+  /**
+   * Scope for user's organization roles per [RFC 0001](https://github.com/logto-io/rfcs) (`organization_roles` claim).
+   *
+   * To learn more about Logto Organizations, see https://docs.logto.io/docs/recipes/organizations/.
+   */
   case organizationRoles = 'urn:logto:scope:organization_roles';
 }
